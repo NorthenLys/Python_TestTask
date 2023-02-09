f4191661fb0d0189b14cc734f58e56027825def7
alice <northenlys@gmail.com>
2023-02-09 16:01:02 +0000

    add Ver1 for TestTask2

---
+++ TestTask2_Hello_name.py
@@ -0,0 +1,7 @@
+def TestTask2():
+    print('Please enter your name')
+    name = input()
+    if name == 'Вячеслав':
+        print('Привет,', name)
+    else:
+        print('Нет такого имени, человек с именем', name)
