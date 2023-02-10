0ba7fd4e02b4285a38e61bbd2c83caf327273dbc
alice <northenlys@gmail.com>
2023-02-10 19:29:35 +0000

    add visualization for finding correct bracket sequence

---
+++ TestTask4_Bracket_sequence.py
@@ -0,0 +1,26 @@
+bracket_seq = str(input()) # Works only with '()' and '[]' type of brackets. Ex. for test [((())()(())]]
+if len(bracket_seq)%2==0:
+    n_start = 0
+    n_end = len(bracket_seq)-1
+    while n_start < n_end:
+        for i in range(len(bracket_seq)):
+            if (bracket_seq[n_start] == '(' or bracket_seq[n_start] == '[') and (bracket_seq[n_end] != bracket_seq[n_start]):
+                if (bracket_seq[n_start] == '(' and bracket_seq[n_end] == ')') or (bracket_seq[n_start] == '[' and bracket_seq[n_end] == ']'):
+                    continue
+                else:
+                    print('False')
+                    exit()
+            elif (bracket_seq[n_start] == bracket_seq[n_end-1]) and (bracket_seq[n_start+1] == bracket_seq[n_end]):
+                continue
+            else:
+                print('False')
+                exit()
+        n_start += 1
+        n_end -= 1
+    print('True')
+elif bracket_seq == ' ':
+    print('True')
+else:
+    print('False')
+
+
