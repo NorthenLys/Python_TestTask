9439eb5a93012ffe43217fd551afda5f04877ab4
alice <northenlys@gmail.com>
2023-02-12 12:43:41 +0000

    add example of fixing brasket sequence

---
+++ TestTask4_How fix brasket seq.py
@@ -0,0 +1,12 @@
+bracket_seq = str(input()) # Works only with '()' and '[]' type of brackets. Ex. for test [((())()(())]]
+new_seq = []
+n_bracket_seq = 0
+for i in range(0,len(bracket_seq)):
+    if bracket_seq[n_bracket_seq] == '(' or bracket_seq[n_bracket_seq]==')':
+       new_seq.append('(')
+       new_seq.append(')')
+    elif bracket_seq[n_bracket_seq] == '[' or bracket_seq[n_bracket_seq]==']':
+         new_seq.append('[')
+        new_seq.append(']')
+    n_bracket_seq+=1
+print(*new_seq)
