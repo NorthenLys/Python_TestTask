740f021e2ec6148e686fda950db5f1cb3c516809
alice <northenlys@gmail.com>
2023-02-20 06:48:18 +0000

    add fix for structure of code

--- TestTask4_How fix brasket seq.py
+++ TestTask4_How fix brasket seq.py
@@ -7,6 +7,6 @@ for i in range(0,len(bracket_seq)):
        new_seq.append(')')
     elif bracket_seq[n_bracket_seq] == '[' or bracket_seq[n_bracket_seq]==']':
          new_seq.append('[')
-        new_seq.append(']')
+         new_seq.append(']')
     n_bracket_seq+=1
 print(*new_seq)
