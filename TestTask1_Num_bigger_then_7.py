9c6599a18abef25c88508d4504def6ac1ea1dc0a
alice <northenlys@gmail.com>
2023-02-24 11:44:24 +0000

    delete old code for tasks

--- TestTask1_Num_bigger_then_7.py
+++
@@ -1,6 +0,0 @@
-print('Please enter the number bigger then 7 for say "Hello"')
-num = float(input())
-if num > 7:
-    print('Солнечного дня, человек!')
-else:
-    print('Число меньше 7. Попробуйте снова.')
--- TestTask2_Hello_name.py
+++
@@ -1,6 +0,0 @@
-print('Please enter your name')
-name = str(input())
-if name == 'Алиса':
-    print('Приветствую, мастер,', name)
-else:
-    print('Имени не существует, человек по имени', name)
